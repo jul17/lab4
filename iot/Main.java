7c41e305034d6cce46843ac4322d66d0adf0293d
jul17 <32940288+jul17@users.noreply.github.com>
2018-03-01 01:19:10 +0200

    Initial commit

---
+++ iot/Main.java
@@ -0,0 +1,18 @@
+package lab4.iot;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("Start Work.. \n");
+
+		StringProcessor stringProcessor = new StringProcessor();
+		String userText = stringProcessor.readInputText();
+		stringProcessor.processText(userText);
+
+		System.out.println("\nDone..");
+	}
+
+}
